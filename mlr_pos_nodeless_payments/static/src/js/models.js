361fc9b8b4cc021c2e0c6d7956ce5c5c4c4614e4
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2023-09-30 18:18:38 -0400

    Delete mlr_pos_nodeless_payments directory

--- mlr_pos_nodeless_payments/static/src/js/models.js
+++
@@ -1,6 +0,0 @@
-odoo.define('pos_nodelesspayment.models', function (require) {
-var models = require('point_of_sale.models');
-var PaymentNodelessPayment = require('pos_nodelesspayment.payment');
-
-models.register_payment_method('nodeless', PaymentNodelessPayment);
-});
